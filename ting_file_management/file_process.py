f2567ffdb7de067c517894377ddd6af6b5b78a81
arthutepio <arthurodriguesr10@gmail.com>
2023-06-06 19:31:02 -0300

    feat: implementa a função process que transforma o conteúdo em um dicionário

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,27 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    position = len(instance)
+
+    for index in range(position):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    lines = txt_importer(path_file)
+
+    if len(lines) > 0:
+        file_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines
+        }
+        instance.enqueue(file_data)
+
+        print(file_data, file=sys.stdout)
+    else:
+        print(f"Arquivo {path_file} está vazio. Ignorando.", file=sys.stdout)
 
 
 def remove(instance):
