8b866d7da28d6c53a021062fdb1a6aadcfde883b
arthutepio <arthurodriguesr10@gmail.com>
2023-06-06 20:48:19 -0300

    feat: implementa a funçao search_by_word que busca uma palavra em todos os arquivos processados

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,4 +20,17 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = exists_word(word, instance)
+
+    for index in range(len(result)):
+        for idx in range(len(instance)):
+            file = instance.search(idx)
+
+            if file["nome_do_arquivo"] == result[index]["arquivo"]:
+                for i in range(len(result[index]["ocorrencias"])):
+                    lines = file["linhas_do_arquivo"]
+                    line_number = result[index]["ocorrencias"][i]["linha"]
+                    content = lines[line_number - 1]
+                    result[index]["ocorrencias"][i]["conteudo"] = content
+
+    return result
