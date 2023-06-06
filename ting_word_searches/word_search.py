5dfb1186e427c7dcde8712e2031ff3858f45c762
arthutepio <arthurodriguesr10@gmail.com>
2023-06-06 20:15:20 -0300

    feat: implementa a função exist_word que verifica a existência de uma palavra em todos os arquivos processados

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for file in range(len(instance)):
+        file = instance.search(file)
+        file_info = {
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": [],
+            }
+
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                file_info["ocorrencias"].append({"linha": index + 1})
+
+        if file_info["ocorrencias"]:
+            results.append(file_info)
+
+    return results
 
 
 def search_by_word(word, instance):
