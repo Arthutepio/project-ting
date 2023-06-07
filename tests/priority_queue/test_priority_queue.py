10590b4dabe512f8cce7c1b353440772da1a96ef
arthutepio <arthurodriguesr10@gmail.com>
2023-06-07 17:19:52 -0300

    feat: implementa testes para a classe PriorityQueue capaz de armazenar arquivos pequenos de forma prioritária

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,21 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+    assert len(queue) == 0
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(6)
+
+    queue.enqueue({"qtd_linhas": 2})
+    assert len(queue) == 1
+
+    queue.enqueue({"qtd_linhas": 7})
+    assert len(queue) == 2
+
+    queue.enqueue({"qtd_linhas": 4})
+    assert queue.dequeue() == {"qtd_linhas": 2}
+
+    assert queue.search(0) == {"qtd_linhas": 4}
